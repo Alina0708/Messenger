3cc3b6d7f82abd0dab870bf826ddc333913d02c1
Alina Sevryuk <alina.sevryuk0708@gmail.com>
2022-08-19 19:51:30 +0300

    modern modal window in news page

--- src/components/News/AddNews/AddNews.jsx
+++ src/components/News/AddNews/AddNews.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import classes from "./AddNews.module.css";
-import * as yup from 'yup';
+import * as yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import ReactTooltip from "react-tooltip";
+import { useState } from "react";
 
 const AddNews = ({ setnewsInfo, newsInfo, setActive }) => {
   const onSubmit = (values, { setSubmitting }) => {
     let newsPost = {
       title: values.titleNews,
       contentNews: values.textNews,
-      image: values.urlImage,
+      image: img,
       likeCount: "65",
     };
     setnewsInfo(newsInfo.push(newsPost));
@@ -20,64 +22,105 @@ const AddNews = ({ setnewsInfo, newsInfo, setActive }) => {
     setActive(false);
   };
 
+  const [img, setImg] = useState();
+
+  const onImageChange = (e) => {
+    const [file] = e.target.files;
+    setImg(URL.createObjectURL(file));
+  };
+
   return (
     <div>
       <h2>Add News</h2>
       <Formik
-        initialValues={{ textNews: "", urlImage: "", titleNews: "" }}
-
+        initialValues={{ textNews: "", titleNews: "" }}
         validate={(values) => {
           const errors = {};
           if (values.titleNews === "") {
-            errors.titleNews = "Write a title. Required!";
-          } else if (values.textNews === "") {
-            errors.textNews = "Write news. Required!";
-          } else if (values.urlImage === "") {
-            errors.urlImage = "Write url image. Required";
+            errors.titleNews = "This field is required";
           }
+          if (values.textNews === "") {
+            errors.textNews = "This field is required";
+          }
+
           return errors;
         }}
         onSubmit={onSubmit}
       >
         {(formikProps) => (
           <Form>
-            <label htmlFor="titleNews">Write title news</label>
+            <div className={classes.titleModal}>
+              <h3 htmlFor="titleNews">
+                Write title news
+                <span className={classes.star} />
+              </h3>
+              <ErrorMessage
+                name="titleNews"
+                // component="div"
+
+                render={(message) => (
+                  <div>
+                    <span className={classes.messageError}>{message}</span>
+                  </div>
+                )}
+              />
+            </div>
+
             <Field
-              className={classes.imageUrl}
+              // data-tip
+              // data-for="errorTitle"
+              className={classes.titleNews}
               name="titleNews"
-              id="titleNews"
               type="text"
               placeholder="Write title news..."
             />
-            <ErrorMessage name="titleNews" component="div" />
 
-            <label htmlFor="urlImage">Write url image</label>
-            <Field
-              className={classes.imageUrl}
-              name="urlImage"
-              id="urlImage"
-              type="text"
-              placeholder="write url image..."
-            />
-            <ErrorMessage name="urlImage" component="div" />
-
-            <label htmlFor="textNews">Write text message</label>
+            <div className={classes.titleModal}>
+              <h3 htmlFor="textNews">
+                Write text message
+                <span className={classes.star} />
+              </h3>
+              <ErrorMessage
+                name="textNews"
+                render={(message) => (
+                  <div>
+                    <span className={classes.messageError}>{message}</span>
+                  </div>
+                )}
+              />
+            </div>
             <Field
               className={classes.news}
               name="textNews"
               as="textarea"
-              placeholder="write news..."
+              placeholder="Write news..."
             />
-            <ErrorMessage name="textNews" component="div" />
+
+            <div>
+              <input
+                // className={classes.buttonInsertImage}
+                type="file"
+                id="urlImage"
+                title="image"
+                onChange={onImageChange}
+              />
+            </div>
 
             <button
-              className={classes.buttonSubmit}
+              className={`${classes.buttonSubmit}
+                ${
+                  (!formikProps.dirty || formikProps.isSubmitting) &&
+                  classes.disabled
+                }`}
               type="submit"
-              disabled={formikProps.isSubmitting}
+              disabled={formikProps.isSubmitting || !formikProps.dirty}
             >
               Submit
             </button>
             <button
+              // className={(navData) =>
+              //   navData.isActive ? classes.activeLink : classes.item
+              // }
               className={classes.buttonCancel}
               type="button"
               onClick={onCancel}
