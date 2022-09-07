f3ba72c67107bdcc902798181f0e4295971cfb3e
Alina Sevryuk <alina.sevryuk0708@gmail.com>
2022-09-07 23:23:07 +0300

    Revert "add worf hello"
    This reverts commit 7e5cbe35c8f7c0632874fbe3818a4f225dfde259.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BackgroundColomns from "./components/BackgroundColomns/BackgroundColomns";
 import { connect } from "react-redux";
 
-//hello 
 const App = (props) => {
   return (
     
